a17e5c0e065f634f09ae7f958c3dd05bd0d644f2
Peter Kowalsky <peter@kowalsky.at>
2018-10-03 17:00:20 +0200

    now with batch requesting :)

--- index.js
+++ index.js
@@ -1,14 +1,40 @@
 exports.handler = (event, context, callback) => {
-  
-  console.log(event.options);
-  const request = require('request');
-
-request({
-  uri: 'https://app.pool4tool.com/login.php',
-  method: 'GET',
-  time: true
-}, (err, resp) => {
-  callback(null, err || resp.timings);
-});
-  
+    const request = require('request');
+    if (event.body.indexOf(";") != -1) {
+        const bason = event.body.split(';');
+        let body = "";
+        let counter = 0;
+        for (var i = 0; i < bason.length; i++) {
+            const incomming_json = JSON.parse(bason[i]);
+            request(incomming_json, (err, resp) => {
+                body = body + ';' + incomming_json.uri + '|' + JSON.stringify(resp.timings || err);
+                counter++;
+                if (counter == bason.length) {
+                    var response = {
+                        "statusCode": 200,
+                        "headers": {
+                            "app": "serverless_HTTPSmon"
+                        },
+                        "body": body,
+                        "isBase64Encoded": false
+                    };
+                    //console.log(response);
+                    callback(null, response);
+                }
+            });
+        }
+    } else {
+        const incomming_json = JSON.parse(event.body);
+        request(incomming_json, (err, resp) => {
+            var response = {
+                "statusCode": 200,
+                "headers": {
+                    "app": "serverless_HTTPSmon"
+                },
+                "body": JSON.stringify(resp.timings || err),
+                "isBase64Encoded": false
+            };
+            callback(null, response);
+        });
+    }
 };

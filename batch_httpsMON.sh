72cc92d7e88bcd40a2069fb8cb79327d541f6467
Peter Kowalsky <peter@kowalsky.at>
2018-10-03 17:11:26 +0200

    formating issues

--- batch_httpsMON.sh
+++ batch_httpsMON.sh
@@ -23,17 +23,17 @@ function getmessurementfromapi() {
 		target=$(echo ${array[$i]} | cut -d'|' -f1)
 		response=$(echo ${array[$i]} | cut -d'|' -f2)
 		hostname=$(echo $api_endpoint | cut -d'/' -f3)
-    socket_time=$(echo $response | cut -d':' -f2 | cut -d',' -f1)
-    lookup_time=$(echo $response | cut -d':' -f3 | cut -d',' -f1)
-    connect_time=$(echo $response | cut -d':' -f4 | cut -d',' -f1)
-    response_time=$(echo $response | cut -d':' -f5 | cut -d',' -f1)
-    total_time=$(echo $response | cut -d':' -f6 | cut -d'}' -f1)
+		socket_time=$(echo $response | cut -d':' -f2 | cut -d',' -f1)
+		lookup_time=$(echo $response | cut -d':' -f3 | cut -d',' -f1)
+		connect_time=$(echo $response | cut -d':' -f4 | cut -d',' -f1)
+		response_time=$(echo $response | cut -d':' -f5 | cut -d',' -f1)
+		total_time=$(echo $response | cut -d':' -f6 | cut -d'}' -f1)
 
-     curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.lookup_time,destination=$target,source=$hostname value=$lookup_time"
-     curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.socket_time,destination=$target,source=$hostname value=$socket_time"
-     curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.connect_time,destination=$target,source=$hostname value=$connect_time"
-     curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.response_time,destination=$target,source=$hostname value=$response_time"
-     curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.total_time,destination=$target,source=$hostname value=$total_time"
+		curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.lookup_time,destination=$target,source=$hostname value=$lookup_time"
+		curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.socket_time,destination=$target,source=$hostname value=$socket_time"
+		curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.connect_time,destination=$target,source=$hostname value=$connect_time"
+		curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.response_time,destination=$target,source=$hostname value=$response_time"
+		curl $CURL_ARGS $INFLUX_DB_LOC --data-binary "lambda_httpsmon.total_time,destination=$target,source=$hostname value=$total_time"
 	done
 	IFS=$OIFS;
 }
